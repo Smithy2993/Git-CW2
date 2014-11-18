d4364a011629923712c43428bd4e87b0ec03f2be
James Smith <sc13jas@leeds.ac.uk>
2014-11-18 19:22:19 +0000

    Venue variables and constructor
    Created instance variables in the Venue class. I also added a
    constructor to the Venue class

--- src/comp2541/coursework/cwk2/Venue.java
+++ src/comp2541/coursework/cwk2/Venue.java
@@ -1,5 +1,29 @@
+/**
+ * The Venue class
+ *
+ * @author James Smith
+ */
+
 package comp2541.coursework.cwk2;
 
-public class Venue
-{
+public class Venue{
+	
+	//Instance variables
+	private String name;
+	private String address;
+	private int phone_number;
+	private String website;
+	private int capacity;
+
+	//Constructor for the variables
+	public Venue(String name, String address, int phone_number, String website,
+			int capacity) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.phone_number = phone_number;
+		this.website = website;
+		this.capacity = capacity;
+	}
+
 }

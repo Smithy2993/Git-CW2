62862d902b5d5a3edd7d06419b3408b11261bef2
James Smith <sc13jas@leeds.ac.uk>
2014-11-18 19:17:36 +0000

    Event variables and constructor
    Created instance variables in the Event class. Also added a constructor
    to the Event class

--- src/comp2541/coursework/cwk2/Event.java
+++ src/comp2541/coursework/cwk2/Event.java
@@ -1,5 +1,31 @@
+/**
+ * The event class
+ *
+ * @author James Smith
+ */
+
 package comp2541.coursework.cwk2;
+
+import org.joda.money.*;
+import org.joda.time.*;
 
 public class Event
 {
+	//Instance Variables
+	private String artists;
+	private LocalDate date;
+	private LocalTime doors_open;
+	private Money price;
+	private int tickets_sold;
+		
+	//Constructor for the variables
+		public Event(String artists, LocalDate date, LocalTime doors_open,
+				Money price, int tickets_sold) {
+			this.artists = artists;
+			this.date = date;
+			this.doors_open = doors_open;
+			this.price = price;
+			this.tickets_sold = tickets_sold;
+		}
+
 }
